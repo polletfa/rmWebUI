a27dfb5cd4cf0cc67d0ffc84de4574db74f4b667
Fabien Pollet <polletfa@posteo.de>
2022-01-03 00:21:32 +0100

    fix include + update screenshots

--- src/backend/Download.php
+++ src/backend/Download.php
@@ -11,7 +11,6 @@
 namespace digitalis\rmWebUI;
 
 require_once __DIR__ . "/../vendor/autoload.php";
-require_once __DIR__ . "/../config/config.php";
 require_once __DIR__ . "/Cache.php";
 
 use splitbrain\RemarkableAPI\RemarkableFS;
