85f97fa954c28eb0b8c2ba822836adb1bd71cfc5
Fabien Pollet <polletfa@posteo.de>
2022-01-12 22:03:35 +0100

    better validate config file

--- src/backend/ConfigLoader.ts
+++ src/backend/ConfigLoader.ts
@@ -42,44 +42,96 @@ export class ConfigLoader {
     }
 
     protected static readServerConfig(yamltree: ParsedYAML, defaultConfig: ServerConfig): ServerConfig {
-        return {
-            name: yamltree && "name" in yamltree ? yamltree.name : defaultConfig.name,
-            port: yamltree && "port" in yamltree && !isNaN(yamltree.port) ? Number(yamltree.port) : defaultConfig.port,
-            demo: yamltree && "demo" in yamltree ? yamltree.demo : defaultConfig.demo,
-            sessionMaxIdle: yamltree && "sessionMaxIdle" in yamltree ?
-                ConfigLoader.readDuration(yamltree.sessionMaxIdle, defaultConfig.sessionMaxIdle) :
-                defaultConfig.sessionMaxIdle,
-            logHeaders: yamltree && "logHeaders" in yamltree ? yamltree.logHeaders : defaultConfig.logHeaders,
+        const data = {
+            name: ConfigLoader.checkString(yamltree, "name", defaultConfig.name),
+            port: ConfigLoader.checkNumber(yamltree, "port", defaultConfig.port),
+            demo: ConfigLoader.checkBoolean(yamltree, "demo", defaultConfig.demo),
+            sessionMaxIdle: ConfigLoader.checkDuration(yamltree, "sessionMaxIdle", defaultConfig.sessionMaxIdle),
+            logHeaders: ConfigLoader.checkBoolean(yamltree, "logHeaders", defaultConfig.logHeaders),
             
-            ssl: {
-                cert: yamltree && "ssl" in yamltree && "cert" in yamltree.ssl ? yamltree.ssl.cert : defaultConfig.ssl.cert,
-                key: yamltree && "ssl" in yamltree && "key" in yamltree.ssl ? yamltree.ssl.key : defaultConfig.ssl.key
-            },           
-            allowInsecure: yamltree && "allowInsecure" in yamltree ? yamltree.allowInsecure : defaultConfig.allowInsecure,
+            allowInsecure: ConfigLoader.checkBoolean(yamltree, "allowInsecure", defaultConfig.allowInsecure),
+
+            ssl: {cert: "", key: ""},
             
-            data: yamltree && "data" in yamltree ? yamltree.data : defaultConfig.data,
-            cache: yamltree && "cache" in yamltree ? yamltree.cache : defaultConfig.cache,
-            pdfconverter: yamltree && "pdfconverter" in yamltree ? yamltree.pdfconverter : defaultConfig.pdfconverter,
+            data: ConfigLoader.checkString(yamltree, "data", defaultConfig.data),
+            cache: ConfigLoader.checkBoolean(yamltree, "cache", defaultConfig.cache),
+            pdfconverter: ConfigLoader.checkString(yamltree, "pdfconverter", defaultConfig.pdfconverter),
             
-            register: yamltree && "register" in yamltree ? yamltree.register : defaultConfig.register,
-            delay: yamltree && "delay" in yamltree ? ConfigLoader.readDuration(yamltree.delay, defaultConfig.delay) : defaultConfig.delay
+            register: ConfigLoader.checkString(yamltree, "register", defaultConfig.register),
+            delay: ConfigLoader.checkDuration(yamltree, "delay", defaultConfig.delay)
         };
+
+        if(yamltree && yamltree["ssl"] !== undefined) {
+            data.ssl = {
+                cert: ConfigLoader.checkString(yamltree.ssl, "cert", defaultConfig.ssl.cert),
+                key: ConfigLoader.checkString(yamltree.ssl, "key", defaultConfig.ssl.key)
+            };
+        }
+        
+        return data;
     }
 
     /**
-     * Read a string as a duration with unit s, m, h or d (default: m) and return the result in milliseconds
+     * Validate a string parameter. Null is accepted and replaced by an empty string
+     *
+     * Return the default value if missing.
+     * Throws an exception if invalid format.
+     * Returns the value otherwise
      */
-    protected static readDuration(str: string, defaultValue: number): number {
-        let unit = str[str.length-1];
-        let value: number;
-        
-        if(unit != "s" && unit != "m" && unit != "h" && unit != "d") {
-            unit = "m";
-            value = Number(str);
+    protected static checkString(yamltree: ParsedYAML, key: string, defaultValue: string): string {
+        if(yamltree && yamltree[key] !== undefined && yamltree[key] !== null)
+            return yamltree[key];
+        else
+            return yamltree[key] === null ? "" : defaultValue;
+    }
+
+    /**
+     * Validate a number parameter. Null is accepted and replaced by 0
+     *
+     * Return the default value if missing.
+     * Throws an exception if invalid format.
+     * Returns the value otherwise
+     */
+    protected static checkNumber(yamltree: ParsedYAML, key: string, defaultValue: number): number {
+        if(yamltree && yamltree[key] !== undefined && yamltree[key] !== null) {
+            if(isNaN(yamltree[key])) throw new Error("Invalid value for "+key+" (not a number)");
+            return yamltree[key];
         } else {
-            value = Number(str.substr(0, str.length-1));
+            return yamltree[key] === null ? 0 : defaultValue;
         }
-        if(isNaN(value)) return defaultValue;
-        return value * (unit == "s" ? 1000 : unit == "m" ? 1000*60 : unit == "h" ? 1000*60*60 : 1000*60*60*24);
+    }
+
+    /**
+     * Validate a boolean parameter. Null is accepted and replaced by false
+     *
+     * Return the default value if missing.
+     * Throws an exception if invalid format.
+     * Returns the value otherwise
+     */
+    protected static checkBoolean(yamltree: ParsedYAML, key: string, defaultValue: boolean): boolean {
+        if(yamltree && yamltree[key] !== undefined && yamltree[key] !== null) {
+            if(typeof yamltree[key] != "boolean") throw new Error("Invalid value for "+key+" (not a boolean)");
+            return yamltree[key];
+        } else {
+            return yamltree[key] === null ? false : defaultValue;
+        }
+    }    
+
+    /**
+     * Read a string as a duration with unit s, m, h or d and return the result in milliseconds
+     */
+    protected static checkDuration(yamltree: ParsedYAML, key: string, defaultValue: number): number {
+        if(yamltree && yamltree[key] !== undefined && yamltree[key] !== null) {
+            const str = String(yamltree[key]);
+            let unit = str[str.length-1];
+            let value = Number(str.substr(0, str.length - 1));
+            
+            if(isNaN(value) || (unit != "s" && unit != "m" && unit != "h" && unit != "d")) {
+                throw new Error("Invalid value for "+key+" (not a duration)");
+            }
+            return value * (unit == "s" ? 1000 : unit == "m" ? 1000*60 : unit == "h" ? 1000*60*60 : 1000*60*60*24);
+        } else {
+            return defaultValue;
+        }
     }
 }
